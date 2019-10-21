67fbd3c7e7949c8a16105016b83446710c540048
duchao <18433216@qq.com>
2019-10-21 17:53:47 +0800

    Gateway 代码初步编写

--- swordsman-gateway/src/main/java/com/swordsman/gateway/GatewayApplication.java
+++ swordsman-gateway/src/main/java/com/swordsman/gateway/GatewayApplication.java
@@ -1,4 +1,13 @@
 package com.swordsman.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+
 }
