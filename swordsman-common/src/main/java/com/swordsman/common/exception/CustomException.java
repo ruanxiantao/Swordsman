4c628881193d5dbf367c0f3a1737a22ae0c5fa25
duchao <18433216@qq.com>
2019-10-22 14:41:28 +0800

    新增自定义状态枚举异常

--- swordsman-common/src/main/java/com/swordsman/common/exception/CustomException.java
+++ swordsman-common/src/main/java/com/swordsman/common/exception/CustomException.java
@@ -1,10 +1,15 @@
 package com.swordsman.common.exception;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 /**
  * @Author DuChao
  * @Date 2019-08-27 16:56
  * 自定义异常
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class CustomException extends RuntimeException{
     public CustomException(String message) {
         super(message);
--- swordsman-common/src/main/java/com/swordsman/common/exception/StatusException.java
+++ swordsman-common/src/main/java/com/swordsman/common/exception/StatusException.java
@@ -1,4 +1,23 @@
 package com.swordsman.common.exception;
 
-public class StatusException {
+import com.swordsman.common.web.Status;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @Author DuChao
+ * @Date 2019-10-22 10:13
+ * 状态枚举异常
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class StatusException extends RuntimeException{
+
+    private Status status;
+
+    public StatusException(Status status){
+        super(status.getMessage());
+        this.status = status;
+    }
+
 }
