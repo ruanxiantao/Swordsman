d38403c9f1104cfa9c314001e3293629e24ee426
duchao <18433216@qq.com>
2019-10-23 17:46:14 +0800

    更新常量类注释

--- swordsman-user/src/main/java/com/swordsman/user/constants/Const.java
+++ swordsman-user/src/main/java/com/swordsman/user/constants/Const.java
@@ -10,15 +10,25 @@ import java.util.List;
  */
 public interface Const {
 
+    /**
+     * 账号状态 : 启用
+     */
     Integer USER_ENABLE = 1;
 
+    /**
+     * Jwt 在Redis 中存储前缀
+     */
     String JWT_PREFIX = "Security_Jwt_";
 
+    /**
+     * 权限类型 : 按钮
+     */
     Integer PERMISSION_BUTTON = 2;
 
+    /**
+     * JwtFilter 忽略路径
+     */
     List<String> IGNORE_SECURITY =
             Arrays.asList( ".css",".js",".html","ui","v2","swagger","v3","png","error","ico","csrf","auth");
-
-    String ANONYMOUS_NAME = "匿名用户";
 
 }
